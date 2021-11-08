0f318c41f2f38d1632c22aad7cd579995970eb0a
Rohit Kumar Singh <rohitt.singh15@gmail.com>
2021-11-08 15:53:39 +0530

    dashboard commit

---
+++ src/test/java/com/developer/hystrix/dashborad/HystrixDashboradApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer.hystrix.dashborad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDashboradApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
